2f35393df5786bec37c14707c631ab504d9c909d
rrigamondi <riccardo1.rigamondi@mail.polimi.it>
2020-09-17 12:19:20 +0200

    Assignment upload
    Dandelion / mouse input + frameCount + noise

--- sketch.js
+++ sketch.js
@@ -5,8 +5,54 @@ function preload(){
 function setup() {
   createCanvas(windowWidth,windowHeight)
   // put setup code here
+  background('black');
+  angleMode(DEGREES);
+  frameRate(12);
+
+  //tutorial
+  mouseval = false;
+  textSize(32)
+  textAlign(CENTER)
+  fill('white')
+  text("Press mouse to grow",width/2,height/4);
+
+  //gambo
+  noFill()
+  stroke('green')
+  strokeWeight(50)
+  let c1x = width/2-width/7;
+  let c1y = height/2+height/4;
+  let c2x = width/2-width/12;
+  let c2y = height/2+height/12;
+  bezier(width/2-width/9,height+5,c1x,c1y,c2x,c2y,width/2,height/2);
+
+  noStroke();
+  fill('white');
+  ellipse(width/2,height/2,100);
 }
 
+
 function draw() {
-  // put drawing code here
+
+  if (mouseIsPressed && mouseval === false){
+    fill('black')
+    rectMode(CENTER)
+    rect(width/2,height/4, 600,200)
+    mouseval = true;
+  }
+
+  let nmax = 100; //max width of piumetta
+  let n = noise((frameCount+random(0,1)))*nmax;
+
+  if (mouseIsPressed) {
+    //gambetto
+    stroke('white');
+    strokeWeight(1);
+    line(width/2,height/2,mouseX,mouseY);
+    //piumetta
+    noStroke();
+    //noFill();
+    fill('rgba(255,255,255,0.6)')
+    strokeWeight(4);
+    ellipse(mouseX,mouseY,n);}
 }
